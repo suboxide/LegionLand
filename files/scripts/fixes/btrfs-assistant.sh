97465ab2b58df29ff1430033031386c9df156c26
suboxide <suboxide@gmail.com>
2024-09-09 15:23:32 +0200

    Delete files/scripts/fixes directory

--- files/scripts/fixes/btrfs-assistant.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -oue pipefail
-
-# Patch btrfs-assistant-launcher to not pass $XDG_RUNTIME_DIR so that it does not break flatpaks
-sed -i 's/ --xdg-runtime=\\"${XDG_RUNTIME_DIR}\\"//g' /usr/bin/btrfs-assistant-launcher
