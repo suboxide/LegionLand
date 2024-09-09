f346a034686ffb555998b561a5585b10307107a1
suboxide <suboxide@gmail.com>
2024-09-09 14:35:39 +0200

    Delete files/scripts/sddm/disable-sddm-virtualkbd.sh

--- files/scripts/sddm/disable-sddm-virtualkbd.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -oue pipefail
-
-# Remove virtual keyboard on SDDM
-# as this image is for workstations and permanently docked steamdecks
-rm /etc/sddm.conf.d/virtualkbd.conf
